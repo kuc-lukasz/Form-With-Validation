dd8c67cd7842fe524c849b8ee2fa05c2c46556f3
lukaq <82107173+kuc-lukasz@users.noreply.github.com>
2021-10-09 07:03:18 +0200

    add information about wrong pass or email or empty field

--- main.js
+++ main.js
@@ -1,35 +1,64 @@
 const form = document.querySelector("form");
-const emailInput = document.querySelector("#email")
-const passInput = document.querySelector('#pass')
-const btnConfirm = document.querySelector('button')
+const emailInput = document.querySelector("#email");
+const passInput = document.querySelector("#pass");
+const btnConfirm = document.querySelector("button");
 
 form.addEventListener("submit", function (e) {
-    e.preventDefault()
-    if (emailInput.value === ""){
-        emailInput.classList.add('red')
-        return
-    } if (passInput.value === ""){
-        passInput.classList.add('red')
-        return
+  e.preventDefault();
+  if (emailInput.value === "") {
+    emailInput.classList.add("red");
+    const alert = document.createElement("div");
+    alert.innerText = "To pole jest wymagane";
+    const boxEmail = document.querySelector(".information-wrong-email");
+    boxEmail.appendChild(alert);
 
-    }
+    return;
+  }
+  if (passInput.value === "") {
+    passInput.classList.add("red");
+    const alert = document.createElement("div");
+    alert.innerText = "To pole jest wymagane";
+    const boxEmail = document.querySelector(".information-wrong");
+    boxEmail.appendChild(alert);
 
-    
-    const displayEmail = document.querySelector('.user-email')
-    const displayPass = document.querySelector('.user-password')
-    displayEmail.innerText = emailInput.value
-    console.log(passInput.value)
-    displayPass.innerText = passInput.value
-    const boxAfterLog = document.querySelector('.box-afterLog')
-    boxAfterLog.classList.remove('hidden')
+    return;
+  } else{
 
+  const displayEmail = document.querySelector(".user-email");
+  const displayPass = document.querySelector(".user-password");
+  displayEmail.innerText = emailInput.value;
+  console.log(passInput.value);
+  displayPass.innerText = passInput.value;
+  const boxAfterLog = document.querySelector(".box-afterLog");
+  boxAfterLog.classList.remove("hidden");
+  }
+});
 
-})
+emailInput.addEventListener("keyup", () => {
+  if (emailInput.value === "") {
+    console.log(emailInput.value);
+    emailInput.classList.add("red");
+    const alert = document.createElement("div");
+    alert.innerText = "To pole jest wymagane";
+    const boxEmail = document.querySelector(".information-wrong-email");
+    boxEmail.appendChild(alert);
 
-emailInput.addEventListener('keyup', ()=> 
-console.log(emailInput.value)
+  } else {
+    emailInput.classList.remove("red");
+  }
+});
 
-)
-passInput.addEventListener('keyup', ()=> {
-    console.log(passInput.value)
-})
+passInput.addEventListener("keyup", () => {
+  if (passInput.value === "") {
+    passInput.classList.add("red");
+    const alert = document.createElement("div");
+    alert.innerText = "To pole jest wymagane";
+    const boxEmail = document.querySelector(".information-wrong");
+    boxEmail.appendChild(alert);
+
+    return;
+  } else {
+    passInput.classList.remove("red");
+  }
+  console.log(passInput.value);
+});
