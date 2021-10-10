6c3b50a99aba81a2c3aee2029203dbc810ea569e
lukaq <82107173+kuc-lukasz@users.noreply.github.com>
2021-10-10 10:34:47 +0200

    new main1.JS file more clear code and prvntdefaul function

---
+++ main1.js
@@ -0,0 +1,13 @@
+const form = document.querySelector('form')
+
+const email = document.querySelector('#email')
+const wrongEmailInfo = document.querySelector('.wrong-email')
+
+const password = document.querySelector('#password')
+const wrongPassInfo = document.querySelector('.wrong-pass')
+
+form.addEventListener('submit', function (e){
+e.preventDefault()
+
+
+})
