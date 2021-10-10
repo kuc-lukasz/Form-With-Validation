b69047485c4b324770f781a8ab85d8772b4d1a28
lukaq <82107173+kuc-lukasz@users.noreply.github.com>
2021-10-10 10:52:07 +0200

    add new container after submit

--- main1.js
+++ main1.js
@@ -1,13 +1,25 @@
-const form = document.querySelector('form')
+const form = document.querySelector("form");
 
-const email = document.querySelector('#email')
-const wrongEmailInfo = document.querySelector('.wrong-email')
+const email = document.querySelector("#email");
+const wrongEmailInfo = document.querySelector(".wrong-email");
 
-const password = document.querySelector('#password')
-const wrongPassInfo = document.querySelector('.wrong-pass')
+const password = document.querySelector("#pass");
+const wrongPassInfo = document.querySelector(".wrong-pass");
 
-form.addEventListener('submit', function (e){
-e.preventDefault()
+const credentialContainer = document.querySelector(".box-afterLog");
 
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  dataAfterSubmit(email, password);
+});
 
-})
+function dataAfterSubmit(email, password) {
+  const emailDisplay = document.querySelector(".user-email");
+  const passwordDisplay = document.querySelector(".user-pass");
+
+  if (credentialContainer.classList.contains("hidden")) {
+    credentialContainer.classList.remove("hidden");
+    emailDisplay.innerText = email.value;
+    passwordDisplay.innerText = password.value;
+  }
+}
